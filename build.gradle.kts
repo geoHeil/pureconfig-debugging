e374b2147793ce9206cca001bae63fa64081966d
Georg Heiler <georg.kf.heiler@gmail.com>
2019-11-27 07:36:56 +0100

    experiment with 2.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,4 +14,7 @@ repositories {
 dependencies {
     implementation("org.scala-lang:scala-library:2.11.12")
     implementation("com.github.pureconfig:pureconfig_2.11:0.12.1")
+
+    //implementation("org.scala-lang:scala-library:2.12.10")
+    //implementation("com.github.pureconfig:pureconfig_2.12:0.12.1")
 }
